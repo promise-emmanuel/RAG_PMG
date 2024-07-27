63a86df5637e92aad893c79fec9fd6d880cccb14
promise-emmanuel-20 <promiseemmanuel2019@gmail.com>
2024-07-27 11:06:54 -0600

    rag_baseline

---
+++ RAG_PMG/app.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.node_parser import JSONNodeParser
+import json
+from flask import Flask, request, render_template
+
+
+
+# Load environment variables
+_ = load_dotenv(find_dotenv())
+app=Flask(__name__)
+
+documents = SimpleDirectoryReader(input_files=["./data/Pmg_lds.md"]).load_data()
+
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+@app.route("/", methods=["POST", 'GET'])
+def index():
+    if request.method == "POST":
+        query = request.form.get("query")
+        response = query_engine.query(query)    
+        return render_template('index.html', query=query, response=response)
+    return render_template('index.html')
+
+if __name__ =="__main__":
+    port = int(os.environ.get('port', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
